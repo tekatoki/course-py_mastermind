1782a67b0c7c5bc067554d0743c86da01dfb4aac
Daniel Gonzalez <danoid2007@gmail.com>
2022-08-09 11:20:06 +0200

    Refactor function to go to the other side
    This refactor may fix some future issues using the operation module to
    transport the player to the other side of the map

--- second_part-course/maze.py
+++ second_part-course/maze.py
@@ -15,22 +15,16 @@ def clear():
 
 
 def print_map(parameter_map_width_x, parameter_map_height_y, parameter_player_coordinate):
-    # The parameter_player_coordinate must be an array
+    # The parameter_player_coordinate must be an array with two ints
 
     # Checks if the player is in the limit of the map
     # If the player is in the limit of the map, it will appear on the other side of the map
 
     # For the coordinate x
-    if parameter_player_coordinate[0] == parameter_map_width_x:
-        parameter_player_coordinate[0] = parameter_map_width_x - parameter_map_width_x
-    elif parameter_player_coordinate[0] == (parameter_map_width_x - parameter_map_width_x):
-        parameter_player_coordinate[0] = parameter_map_width_x - 1
+    parameter_player_coordinate[0] %= parameter_map_width_x
 
     # For the coordinate y
-    if parameter_player_coordinate[1] == parameter_map_height_y:
-        parameter_player_coordinate[1] = parameter_map_height_y - parameter_map_height_y
-    elif parameter_player_coordinate[1] == parameter_map_height_y - parameter_map_height_y:
-        parameter_player_coordinate[1] = parameter_map_height_y - 1
+    parameter_player_coordinate[1] %= parameter_map_height_y
 
     print('+' + '-' * parameter_map_width_x * 3 + '+')
 
